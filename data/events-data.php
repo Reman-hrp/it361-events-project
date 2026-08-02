8edeaf56cd08fbff5f263551a1a16b0f7edcc0c1
reman-hrp <Reman18161@gmail.com>
2026-08-03 01:53:35 +0300

    Add events data file and images folder

---
+++ data/events-data.php
@@ -0,0 +1,49 @@
+<?php
+
+$events = [
+
+    [
+        "eventId" => 1,
+        "eventTitle" => "Introduction To Web Development",
+        "eventCategory" => "Workshop",
+        "eventDate" => "August 5, 2026",
+        "eventTime" => "10:00 AM",
+        "eventLocation" => "IT Building",
+        "eventImage" => "images/image1.png",
+        "eventDescription" => "A workshop organized by the university for its students to teach them the fundamentals of basic programming languages ​​for developing modern websites, such as HTML and CSS."
+    ],
+
+    [
+        "eventId" => 2,
+        "eventTitle" => "Career Development Seminar",
+        "eventCategory" => "Seminar",
+        "eventDate" => "August 18, 2026",
+        "eventTime" => "1:00 PM",
+        "eventLocation" => "University Auditorium",
+        "eventImage" => "images/image2.png",
+        "eventDescription" => "A seminar focused on empowering students to create and enhance their resumes, as well as developing their interview skills."
+    ],
+
+    [
+        "eventId" => 3,
+        "eventTitle" => "Data Analytics Competition",
+        "eventCategory" => "Competition",
+        "eventDate" => "August 25, 2026",
+        "eventTime" => "11:00 AM",
+        "eventLocation" => "Business Building",
+        "eventImage" => "images/image3.png",
+        "eventDescription" => "A competition where students face off in solving data analysis challenges, relying on teamwork and analytical thinking."
+    ],
+
+    [
+        "eventId" => 4,
+        "eventTitle" => "Technology Innovation Trip",
+        "eventCategory" => "Trip",
+        "eventDate" => "September 2, 2026",
+        "eventTime" => "9:00 AM",
+        "eventLocation" => "Innovation Center",
+        "eventImage" => "images/image4.png",
+        "eventDescription" => "An educational trip for university students to explore emerging technologies and learn how to use them."
+    ]
+
+];
